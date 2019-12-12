7f67ba390da56f382b528cb9e8e29c9cd75144d7
Hans Foster <fosterhans@gmail.com>
2019-12-12 20:26:42 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,11 @@ class ApplicationController < Sinatra::Base
   
   class Dog
     
-    attr_accessor :name, :
+    attr_accessor :name, :breed, :age 
+    
+    def initialize(name, breed, age)
+      @name = name 
+      @breed = breed 
+      @age = age
   end
 end
