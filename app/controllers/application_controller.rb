0a650d6420fed1a73b9496c4ccb0f44e3c68c75a
Hans Foster <fosterhans@gmail.com>
2019-12-12 20:29:08 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,12 +15,11 @@ class Dog
     
   attr_accessor :name, :breed, :age 
     
-    def initialize(name, breed, age)
-      @name = name 
-      @breed = breed 
-      @age = age
-    end
-    
+  def initialize(name, breed, age)
+    @name = name 
+    @breed = breed 
+    @age = age
   end
-  
+    
 end
+
